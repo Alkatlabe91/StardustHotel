d15662417c5e31c4590a4b4bc944808a157a3052
“Alaa <1074002@hr.nl>
2024-06-10 01:00:14 +0200

    update the project

--- hotel1.sql
+++ hotel1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.3.0-dev+20220523.649d9b34ea
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2022 at 09:39 PM
+-- Generation Time: Jun 10, 2024 at 12:14 AM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.4
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,10 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hotel`
+-- Database: `hotel1`
 --
-CREATE DATABASE IF NOT EXISTS `hotel` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `hotel`;
 
 -- --------------------------------------------------------
 
@@ -29,11 +27,8 @@ USE `hotel`;
 -- Table structure for table `reservation`
 --
 
-DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE `reservation` (
   `id` int(11) NOT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `roomid` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `surname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -44,17 +39,19 @@ CREATE TABLE `reservation` (
   `total` double DEFAULT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `roomid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `reservation`
 --
 
-INSERT INTO `reservation` (`id`, `userid`, `roomid`, `name`, `surname`, `email`, `phone`, `checkin`, `checkout`, `days`, `total`, `status`, `created_at`, `updated_at`) VALUES
-(1, 15, 18, 'max', 'max', 'Max@max.com', '11111111', '2017-01-01 00:00:00', NULL, 3, 750, 'Cancelled', NULL, NULL),
-(2, 15, 1, 'max', 'max', 'Max@max.com', '1111111111111111', '2017-01-01 00:00:00', NULL, 2, 180, 'Completed', NULL, NULL),
-(17, 20, 22, 'aa', 'a', 'a@a.com', '999999999999', '2022-06-30 00:00:00', '2022-07-04 00:00:00', 4, 1600, 'Accepted', '2022-06-07 13:54:55', NULL);
+INSERT INTO `reservation` (`id`, `name`, `surname`, `email`, `phone`, `checkin`, `checkout`, `days`, `total`, `status`, `created_at`, `updated_at`, `userid`, `roomid`) VALUES
+(22, 'Zaka', 'Zaka', 'Alaa@Alaa.com', '11111111111', '2022-06-24 00:00:00', '2022-06-27 00:00:00', 3, 270, 'Accepted', '2022-06-23 12:02:56', NULL, NULL, NULL),
+(23, 'max', 'max', 'Max@max.com', '1111111111', '2022-06-09 00:00:00', '2023-01-17 00:00:00', 222, 19980, 'New', '2022-06-23 22:01:29', NULL, 15, 1),
+(24, 'Nouar', 'Sebahe', 'Nouar@gmail.com', '0999555444', '2024-05-05 00:00:00', '2024-05-08 00:00:00', 3, 270, 'New', '2024-05-03 20:41:20', NULL, 25, 1);
 
 -- --------------------------------------------------------
 
@@ -62,7 +59,6 @@ INSERT INTO `reservation` (`id`, `userid`, `roomid`, `name`, `surname`, `email`,
 -- Table structure for table `room`
 --
 
-DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -78,7 +74,7 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`id`, `title`, `description`, `image`, `price`, `status`, `number`) VALUES
-(1, 'Comfort Room', 'Room size: 28 sqm', '4e3cffeb173557c4fce21201fcacfda3.jpg', 90, 'New', 1),
+(1, 'Comfort Room', 'Room size: 28 sqm', '4e3cffeb173557c4fce21201fcacfda3.jpg', 90, 'Available', 1),
 (18, 'Superior Room', 'Room size: 26 sqm, Suitable for 2 guests, Twin beds', '51c8e5280a6f86aa92c3e1e915cb6713.jpg', 250, 'new', 2),
 (19, 'Superior Plus Room', 'Room size: 26 sqm, undefined\r\nSuitable for 2 guests, King-size bed', 'b6048f5a3f3eacb6add165fb9bcc99bb.jpg', 219, 'new', 3),
 (20, 'Deluxe King Room', 'Room size: 31-35 sqm, Up to 3 people, King-size bed', '3edd292d16131c111f2dd9fc9e415ba7.jpg', 320, 'new', 4),
@@ -93,7 +89,6 @@ INSERT INTO `room` (`id`, `title`, `description`, `image`, `price`, `status`, `n
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -112,12 +107,15 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `roles`, `password`, `name`, `surname`, `image`, `status`, `created_at`, `updated_at`) VALUES
-(14, 'admin@admin.com', '[\"ROLE_ADMIN\"]', '$2y$13$waYNTCYfQKD25Qo/UChHROPltIe7zABMRgOiAQranjHxVnpNwFAWi', 'admin', 'admin', '4bc3e89d45aa62b21cd97280d3f5c547.png', 'True', NULL, NULL),
-(15, 'Max@max.com', '[\"ROLE_USER\"]\n', '$2y$13$hbEscHE..PNlLvN9b4NxW.CFDMWNxoHaIkbYLxkHGxg6D/52La2mi', 'max', 'max', 'd55fe609752c8149c7c6928e0c64913c.png', 'False', NULL, NULL),
-(16, 'sam@sam.com', '[\"ROLE_USER\"]\n', '$2y$13$subtU6VYw866Qa2PASOYwuQS9neZUTcdn4dLC3Zdbi0W.tYRTsSqG', 'sam', 'sam', '5639e14aa1dba5a085a235b10e69cfaf.png', 'False', NULL, NULL),
-(17, 'rami@r.com', '[]', '$2y$13$UiY7TT5nifuaILvgEn5OoOWNP4YAu/YLUmqLNisNWdNqS6YwgXfdC', 'rami', 'rami', '234a262cd114e67a0a8f4e122501638e.png', 'False', NULL, NULL),
-(18, 'rafl@ralf.com', '[]', '$2y$13$IxlQECXeo/hOdyPgX5qShOd.GgbqQPit/j8CcjKKwew6YvrUbhc9u', 'ralf', 'ralf', '755faeb2ec7d9b11219facfc06edcf92.png', 'True', NULL, NULL),
-(20, 'a@a.com', '[\"ROLE_MANGER\"]', '$2y$13$O1qApYRYEWdNMs3L8npapuPhWzXTL/wKt6iZpLk1k027ov9Z7bsUS', 'aa', 'a', '7db42dd9da27c9f67211cd80972613e3.png', NULL, NULL, NULL);
+(14, 'admin@admin.com', '[\"ROLE_ADMIN\"]', '$2y$13$EHrUlmm8ibB2jI41TjOg3..OFZyEwDFh0bnzmmrIAziUJpR6w1Gsu', 'admin', 'admin', '395e9f3713c952a6993ca460f6b5f037.png', 'True', NULL, NULL),
+(15, 'Max@max.com', '[\"ROLE_USER\"]\n', '$2y$13$3BuQM67WZgj43cGptNqVbOmq2ycPhqSBQyLAtFqUQm.u/Ai51yfcy', 'max', 'max', '1d9520f74af00d3d31214dc147f60922.png', 'False', NULL, NULL),
+(16, 'sam@sam.com', '[\"ROLE_BALIE\"]', '$2y$13$dQ11V/cmvNjW5db6AGsWa.u.92SYfdAFVuIIDuJReE1rUXT4bmZg6', 'sam', 'sam', 'a9af16ed840150a7f8bbbb6a350d117f.png', 'False', NULL, NULL),
+(22, 'Alaa@Alaa.com', '[]', '$2y$13$OwDnMsicoUR7R7PwbskU8.NHU01ljxbXA.3tggWfp9/Ml5ammr2Ge', 'Zaka', 'Zaka', '314fe893976a41b6f83d7167171baf27.png', NULL, NULL, NULL),
+(23, 'Leo@gmail.com', '[]', '$2y$13$DBoqb1LILMzAWlpf2zUfLenfRYGc6soQ6zmjR2L3HfNjGwrmnmtbq', 'leo', 'leo', 'f24bd03978d95529cac408ea399ff8ef.png', NULL, NULL, NULL),
+(24, 'king@gmail.com', '[]', '$2y$13$Dvw6vskiTudW/FWcZldisOqjNdBIptFGYOb4NbDHdoqw/Wl2S1cLi', 'king', 'king', '73d279d381bc4c83525a2f6d75b35715.png', NULL, NULL, NULL),
+(25, 'Nouar@gmail.com', '[]', '$2y$13$98gfL/OH1Q3OG6ZjF.KVDeomS.Jn0g2HvKmkp4LOhThApuvrS1OhW', 'Nouar', 'Sebahe', '4834b46bc94cf7311528e8cf107821fb.png', NULL, NULL, NULL),
+(26, 'test@gmail.com', '[]', '$2y$13$DYcZMP5IzdQa.fEii/1K0.RAmN0w8Cph7/OSBLmYEj9BfaWI5CUde', 'test', 'test', NULL, NULL, NULL, NULL),
+(27, 'alaa@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$DHJ/UCIsNx62YHW9pZ9aUOzXbCQGTNCIC1fHS9E.7/SEW6UMC9oTS', 'alaa', 'alaa', NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -150,7 +148,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `room`
@@ -162,12 +160,9 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
